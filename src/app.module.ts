875cb381637ae606fca11067456c2b18ce4560eb
OProgramadorJunior <107969523+OProgramadorJunior@users.noreply.github.com>
2022-06-27 21:10:18 -0300

    add conexao com o mongodb;

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { TipoNegocioModule } from './tipo-negocio/tipo-negocio.module';
 import { TipoImovelModule } from './tipo-imovel/tipo-imovel.module';
 import { ImovelModule } from './imovel/imovel.module';
 import { LeadModule } from './lead/lead.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ImovelModule, TipoNegocioModule, TipoImovelModule, LeadModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mongodb',
+    url: process.env.MONGODB_CONNECTION_STRING,
+    database: process.env.MONGODB_DATABASE,
+    entities: [
+      __dirname + '/**/*.entity{.ts,.js}',
+    ],
+    ssl: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }), ImovelModule, TipoNegocioModule, TipoImovelModule, LeadModule],
   controllers: [AppController],
   providers: [AppService],
 })
