a039ccd9a55d94d32975271bb15c3dba40d66dd6
OProgramadorJunior <107969523+OProgramadorJunior@users.noreply.github.com>
2022-06-30 20:11:47 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,21 @@ import { LeadModule } from './lead/lead.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mongodb',
-    url: process.env.MONGODB_CONNECTION_STRING,
-    database: process.env.MONGODB_DATABASE,
-    entities: [
-      __dirname + '/**/*.entity{.ts,.js}',
-    ],
-    ssl: true,
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  }), ImovelModule, TipoNegocioModule, TipoImovelModule, LeadModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+    ImovelModule,
+    TipoNegocioModule,
+    TipoImovelModule,
+    LeadModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
